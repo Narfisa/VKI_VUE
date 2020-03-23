b259b9665a39ddbe38cefb3a1fdfac3a6f3b0497
Валентина Окулова <valya.okulova@mail.ru>
2020-03-23 14:33:21 +0700

    installed axios + methods in vuex for messages

--- chat/src/store/index.ts
+++ chat/src/store/index.ts
@@ -3,12 +3,41 @@ import Vuex from 'vuex';
 
 Vue.use(Vuex);
 
+const API = "http://localhost:8081"
+const axios = require('axios');
+
 export default new Vuex.Store({
   state: {
+    messages: Array<Object>()
+  },
+  getters: {
+    messagesCount: function(state){
+      return state.messages.length
+    }
   },
   mutations: {
+    SET_MESSAGES: function(state, messages) {
+      state.messages = messages
+    },
+    ADD_MESSAGE: function(state, message: Object) {
+      state.messages.push(message) 
+    }
   },
   actions: {
+    receiveMessages: function ({commit}) {
+      axios.get(API).then((response: any) => { // type "any" is needed to be changed
+        commit("setMessages", response)
+      })
+    },
+    sendMessage: function({commit}, message){
+      axios.post(API, JSON.stringify({
+        message: message.message,
+        nickname: message.nickname
+      }))
+      .then(() => {
+        commit("ADD_MESSAGE", message)
+      })
+    }
   },
   modules: {
   },
