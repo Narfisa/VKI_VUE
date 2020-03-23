3ead2bddfb7872d1e46b149ffefa03fc7b4b7b20
Валентина Окулова <valya.okulova@mail.ru>
2020-03-23 13:51:14 +0700

    Initial + API methods

---
+++ chat_back/README.md
@@ -0,0 +1,19 @@
+# crm
+
+## First: install node js
+```
+See https://nodejs.org/en/.
+```
+
+### Second: install express server to project folder with
+```
+npm install express --save
+```
+
+### Third: Run this one in terminal in the project folder
+```
+node server.js
+```
+
+
+### Follow my Vue repository to see front for this project
---
+++ chat_back/server.js
@@ -0,0 +1,30 @@
+let express = require('express');
+let bodyParser = require('body-parser');
+let cors = require('cors');
+
+let server = express();
+let data = []
+
+server.use(cors());
+server.use(bodyParser.json());
+server.use(bodyParser.urlencoded({
+  extended: true
+}));
+
+server.listen(8081, function(){
+  console.log('IT\'S WORKING!');
+})
+
+server.post('/message', function(req, res) {
+  let message = {
+    id: Data.now(),
+    nickname: req.body.nickname,
+    message: req.body.message
+  }
+  data.push(message)
+  res.send(message.id).status(200)
+});
+
+server.get('/message', function(req, res) {
+  res.send(data)
+});
