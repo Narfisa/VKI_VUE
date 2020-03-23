3f26ed2435165fc6bdc6fedbba16198cd6532b7b
Валентина Окулова <valya.okulova@mail.ru>
2020-03-23 14:43:40 +0700

    Created messageList component

---
+++ chat/src/components/MessageList.vue
@@ -0,0 +1,22 @@
+<template>
+    <div class="MessageList">
+        <ul v-if="length">
+            <message v-for="message in messages" :key="message.id"/>
+        </ul>
+    </div>
+</template>
+
+<script lang="ts">
+import Vue from 'vue'
+import Component from 'vue-class-component'
+import Message from '@/components/Message.vue'
+
+export default Vue.extend({
+    components: {
+        Message
+    }
+})
+</script>
+
+<style scoped>
+</style>
