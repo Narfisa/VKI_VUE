07a0ca0cd514d640c172d41a5ca0aae52dfb8908
Валентина Окулова <valya.okulova@mail.ru>
2020-03-23 15:20:43 +0700

    added element io to main.ts

--- chat/src/main.ts
+++ chat/src/main.ts
@@ -2,8 +2,11 @@ import Vue from 'vue';
 import App from './App.vue';
 import router from './router';
 import store from './store';
+import elem from 'element-ui'
 
 Vue.config.productionTip = false;
+
+Vue.use(elem)
 
 new Vue({
   router,
