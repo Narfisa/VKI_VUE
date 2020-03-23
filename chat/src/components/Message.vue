9d7b9ea48ce3230a624bd0b6c0a8c78a67b8de50
Валентина Окулова <valya.okulova@mail.ru>
2020-03-23 14:16:28 +0700

    created message component

---
+++ chat/src/components/Message.vue
@@ -0,0 +1,17 @@
+<template>
+    <div class="message">
+        <span class="nick"> {{ message.nickname }} :</span>
+        <span class="msg"> {{ message.message }} </span>
+    </div>    
+</template>
+
+<script lang="ts">
+import Vue from 'vue'
+export default Vue.extend({
+    props: ['message'],
+})
+</script>
+
+<style scoped>
+
+</style>
