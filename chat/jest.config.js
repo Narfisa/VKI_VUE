1521c5704b46af3870e16962eeb17457827126ee
elensiya <valya.okulova@mail.ru>
2020-04-24 13:58:06 +0700

    fix jest config

--- chat/jest.config.js
+++ chat/jest.config.js
@@ -2,5 +2,8 @@ module.exports = {
   preset: '@vue/cli-plugin-unit-jest/presets/typescript-and-babel',
   "moduleNameMapper": {
     "\\.(css|less)$": "<rootDir>/mockStyle.js"
-  }
+  },
+  collectCoverage: true,
+  collectCoverageFrom: ['**/*.{js,vue}', '!**/node_modules/**'],
+  coverageReporters: ['html', 'text-summary']
 }
