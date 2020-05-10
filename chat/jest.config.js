8c779d4f4aa78e49385f8d6132e5955eea5786b9
elensiya <valya.okulova@mail.ru>
2020-05-10 16:25:34 +0700

    deleted tests [2]

--- chat/jest.config.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-  preset: '@vue/cli-plugin-unit-jest/presets/typescript-and-babel',
-  "moduleNameMapper": {
-    "\\.(css|less)$": "<rootDir>/mockStyle.js"
-  },
-  collectCoverage: true,
-  collectCoverageFrom: ['**/*.{js,vue}', '!**/node_modules/**'],
-  coverageReporters: ['html', 'text-summary']
-}
